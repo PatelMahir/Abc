b80d567a2c558a65f52468b5c02b9370c5289fe5
RAHI PATEL <mahir17062002@gmail.com>
2024-10-22 17:49:20 +0530

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+//import { Component,inject } from './calculator.service';
+@Injectable({
+  providedIn:'root'
+})
+export class CalculatorService
+{
+  add(x:number,y:number)
+  {
+    return x+y;
+  }
+}
+@Component({
+  selector: 'app-receipt',
+  template: `
+  /*<h1>Your Receipt {{totalCost}}</h1>*/
+  `,
+})
+export class Receipt
+{
+  //private cal=inject(CalculatorService);
+  //totalCost=this.calculatorService.add(50,25);
+}
+export interface SignUpForm
+{
+  title:'Sign Up';
+}
 @Component({
   selector: 'app-root',
   //selector:'sign-up-form',
   standalone: true,
   imports: [RouterOutlet],
   template:`
+  @if(isAdmin)
+  {
+    <button> Erase Everything</button>
+  }
   <p>Title:{{taskTitle}}</p>
   <button type="submit" [disabled]="fromIsInvalid">Submit</button>
   `,
@@ -32,3 +62,16 @@ export class AppComponent {
     this.taskTitle=newTitle;
   }
 }
+export class TextTransformer
+{
+  announcement='Hello';
+  transformText()
+  {
+    this.announcement=this.announcement.toUpperCase();
+  }
+}
+@Component({
+  selector:'profile-photo',
+  template: `<img src="profile-photo.jpg" alt="Your profile photo">`,
+})
+export class ProfilePhoto{}
