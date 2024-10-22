9ff05ef09b3cf90c70fdc7522f81db0021f33149
RAHI PATEL <mahir17062002@gmail.com>
2024-10-22 16:46:28 +0530

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,32 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  //selector:'sign-up-form',
   standalone: true,
   imports: [RouterOutlet],
+  template:`
+  <p>Title:{{taskTitle}}</p>
+  <button type="submit" [disabled]="fromIsInvalid">Submit</button>
+  `,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+export class SignUpForm{
+  formIsInvalid=true;
+}
+export class AppBanner{
+  testId='main-cta';
+}
 export class AppComponent {
   title = 'Abc';
+  taskTitle='A Cup of Coffee';
+  isComplete=false;
+  CompleteTask()
+  {
+    this.isComplete=true;
+  }
+  UpdateTitle(newTitle:string)
+  {
+    this.taskTitle=newTitle;
+  }
 }
